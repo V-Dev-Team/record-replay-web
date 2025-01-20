893adff658098276988266d07d42d7e71f74b352
Sahircan Sürmeli <ssahircan@gmail.com>
2025-01-20 20:22:19 +0100

    build for windows

--- record.py
+++ record.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions() 
+bundle_dir = Path(__file__).parent
+
+options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
 options.add_experimental_option("useAutomationExtension", False)
-options.add_extension("./rrweb-extension.zip")
+options.add_extension(bundle_dir / "rrweb-extension.zip")
 
 driver = webdriver.Chrome(options=options)
 
